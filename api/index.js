922b9e05c2ab5722990535460015a1af950acd89
pkhaneff <nguyenminhlong1918@gmail.com>
2024-12-21 14:05:49 +0700

    fix cors middleware

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors({
     origin: process.env.CLIENT_URL || 'http://localhost:5173',  
     credentials: true,                                         
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],      
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",      
     allowedHeaders: ['Content-Type', 'Authorization'],         
 }));
 
