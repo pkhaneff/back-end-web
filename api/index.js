2f830e65742bde318c2a1c04314b86b5574ed296
pkhaneff <nguyenminhlong1918@gmail.com>
2024-12-21 13:58:08 +0700

    update Cors

--- api/index.js
+++ api/index.js
@@ -10,11 +10,12 @@ dotenv.config()
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || '*',  
-    credentials: true,                      
-    methods: '*',                           
-    allowedHeaders: '*',                
-}))
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',  
+    credentials: true,                                         
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],      
+    allowedHeaders: ['Content-Type', 'Authorization'],         
+}));
+
 
 app.use(express.json())
 app.use(cookieParser())
