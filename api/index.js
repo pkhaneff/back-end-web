663c005514360128da8923c98b43518dee305471
pkhaneff <nguyenminhlong1918@gmail.com>
2024-12-30 20:57:28 +0700

    fix cors

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ dotenv.config()
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173'];
 const corsOptions = {
     origin: (origin, callback) => {
         if (!origin || allowedOrigins.includes(origin)) {
