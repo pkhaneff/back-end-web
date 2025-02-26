debfabcd115e8cbca8f7cdb12e9c5b8462204ee9
pkhaneff <nguyenminhlong1918@gmail.com>
2025-02-26 16:19:31 +0700

    final-v.2 test-5

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -27,9 +27,9 @@ export const signup = async (req, res, next) => {
 }
 
 export const signin = async (req, res, next) => {
-    {email, password} = req.body
+    const {email, password} = req.body
 
-    if(!email !password || email === '' || password === ''){
+    if(!email || !password || email === '' || password === ''){
         next(errorHandler(400, 'All fields are required'))
     }
 
