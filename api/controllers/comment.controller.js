c284b4171aabe65f838dd368bf66b98e594e0c4a
pkhaneff <nguyenminhlong1918@gmail.com>
2025-02-27 11:11:36 +0700

    final-v.3 test-13

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -21,7 +21,7 @@ export const createcomment = async (req, res, next) => {
     }
 }
 
-export const = async (req, res, next) => {
+export const getpostcomment = async (req, res, next) => {
     try {
         const comments = await Comment.find({postId: req.params.postId}).sort({createdAt: -1})
         res.status(200).json(comments)
