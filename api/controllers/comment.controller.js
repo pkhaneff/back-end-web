02f66f17bb8967f32618c961abd24f5506c400b1
pkhaneff <nguyenminhlong1918@gmail.com>
2025-02-28 08:51:17 +0700

    pytest-5 test-2

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,7 +1,7 @@
 import Comment from "../models/comment.model.js"
 import { errorHandler } from "../utils/error.js"
 
-export const createcomment = async (req, res, next) => {
+export const = async (req, res, next) => {
     try {
         const {content, postId, userId} = req.body
         if(userId !== req.user.id){
