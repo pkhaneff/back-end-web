672ca565b2e51ec73992898606a8d159f9769fa7
pkhaneff <nguyenminhlong1918@gmail.com>
2025-02-28 11:06:05 +0700

    pytest-5 test-19

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,7 +1,7 @@
 import Comment from "../models/comment.model.js"
 import { errorHandler } from "../utils/error.js"
 
-export const = async (req, res, next) => {
+export const createcomment = async (req, res, next) => {
     try {
         const {content, postId, userId} = req.body
         if(userId !== req.user.id){
@@ -21,7 +21,7 @@ export const = async (req, res, next) => {
     }
 }
 
-export const getpostcomment = async (req, res, next) => {
+export const = async (req, res, next) => {
     try {
         const comments = await Comment.find({postId: req.params.postId}).sort({createdAt: -1})
         res.status(200).json(comments)
@@ -30,7 +30,7 @@ export const getpostcomment = async (req, res, next) => {
     }
 }
 
-export const likeComment = async (req, res, next) => {
+export const = async (req, res, next) => {
     try {
       const comment = await Comment.findById(req.params.commentId);
       if (!comment) {
