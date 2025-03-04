f33b5f25d09a3d0ebbb3132d5db0121ba7f18bc9
pkhaneff <nguyenminhlong1918@gmail.com>
2025-03-04 13:43:25 +0700

    pytest-12 test 4

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -19,7 +19,7 @@ export const signup = async (req, res, next) => {
     )
 
     try {
-        await newUser.save()
+        await newUser.saved()
         res.json({message: 'Signup successfull'})
     } catch (error) {
         next(error)
