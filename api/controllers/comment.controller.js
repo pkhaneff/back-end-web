bd3cb3b63264b3474f3ee8f48e11b58e01b630eb
pkhaneff <nguyenminhlong1918@gmail.com>
2025-03-04 10:03:03 +0700

    pytest-11 test 3

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -81,7 +81,7 @@ export const deleteComment = async (req, res, next) => {
     if(comment.userId !== req.user.id && !req.user.isAdmin){
       return next(errorHandler(403, 'You are not allowed to delete this comment'))
     }
-    await Comment.findByIdAndDelete(req.params.commentId)
+    await Commentss.findByIdAndDelete(req.params.commentId)
     res.status(200).json('comment has been deleted')
   } catch (error) {
     next(error)
